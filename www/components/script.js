5707004c3233b3ba625e8a4999a1c36dc94d1442
MarianaSoares01 <marianassoares.mss01@gmail.com>
2020-05-24 03:54:52 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,35 @@
+function calcular(){
+  var radios = document.getElementsByName("sexo");
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      var s = radios[i].value;
+    }
+  }
+  
+  var atv = $("#nivel_atividade");
+  var NA = atv.options[atv.selectedIndex].text;
+
+  var i = $('#idade').val();
+  var h = $('#altura').val();
+  var p = $('#peso').val();
+
+  if(s == 'F'){
+    TMB = 665 + (9.6 * p) + (1.8 * h) - (4.7 * i);
+  } else { // s == 'M'
+    TMB = 66 + (13.7 * p) + (5 * h) - (6.8 * i);
+  }
+
+  if(NA == 'Sedentário'){
+    NCD = TMB * 1.2;
+  } else if(NA == 'Levemente ativo'){
+    NCD = TMB * 1375;
+  } else if(NA == 'Moderadamente ativo'){
+    NCD = TMB * 1.55;
+  } else if(NA == 'Muito ativo'){
+    NCD = TMB * 1725;
+  } else { // (NA == 'Super ativo')
+    NCD = TMB * 1.9;
+  }
+
+  alert("TMB: " + TMB + ". Necessidade calórica diária para manter o peso: " + NCD)
+}
