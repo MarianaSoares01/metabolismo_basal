02f5de8e6b914400b23ec8aaddd098d6007a9239
MarianaSoares01 <marianassoares.mss01@gmail.com>
2020-05-30 03:58:14 +0900

    Inserção de pluggins

--- www/components/script.js
+++ www/components/script.js
@@ -1,4 +1,4 @@
-function calcular(){
+function calcular(){  
   var radios = document.getElementsByName("sexo");
   for (var i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
@@ -14,22 +14,38 @@ function calcular(){
   var p = $('#peso').val();
 
   if(s == 'F'){
-    TMB = 665 + (9.6 * p) + (1.8 * h) - (4.7 * i);
+    TMB = (665 + (9.6 * p) + (1.8 * h) - (4.7 * i));
   } else { // s == 'M'
-    TMB = 66 + (13.7 * p) + (5 * h) - (6.8 * i);
+    TMB = (66 + (13.7 * p) + (5 * h) - (6.8 * i));
   }
 
   if(NA == 'Sedentário'){
-    NCD = TMB * 1.2;
+    NCD = (TMB * 1.2);
   } else if(NA == 'Levemente ativo'){
-    NCD = TMB * 1375;
+    NCD = (TMB * 1.375);
   } else if(NA == 'Moderadamente ativo'){
-    NCD = TMB * 1.55;
+    NCD = (TMB * 1.55);
   } else if(NA == 'Muito ativo'){
-    NCD = TMB * 1725;
+    NCD = (TMB * 1.725);
   } else { // (NA == 'Super ativo')
-    NCD = TMB * 1.9;
+    NCD = (TMB * 1.9);
   }
 
-  alert("TMB: " + TMB + ". Necessidade calórica diária para manter o peso: " + NCD)
+  navigator.vibrate([8000]);
+  navigator.notification.beep(2);
+  
+  function confirmando(buttonIndex) {
+    if(buttonIndex == 1){
+      alert('Sua Necessidade Calórica Diária é: ' + NCD);
+    } else {
+      alert('Obrigado!');
+    }
+  }
+  
+  navigator.notification.confirm(
+      TMB + 'Deseja saber sua Necessidade Calórica Diária?', // message
+      confirmando,            // callback to invoke with index of button pressed
+      'Sua TMB é: ',           // title
+      ['Sim','Não']     // buttonLabels
+  );
 }
